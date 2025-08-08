dd890c7f9c7ed1c79c9e1f2fabaf99353a044d8e
Mikeymoo5 <moodids@outlook.com>
2025-08-08 17:30:44 -0500

    Added Dungeon and Floor resources
    Added dungeons and floor resources, so that generating a new dungeon creates new floors, which have their own seeds, during floor pregen

--- floor.gd
+++ floor.gd
@@ -3,5 +3,34 @@ extends Resource
 ## A floor of a dungeon.
 class_name Floor
 
-func _init() -> void:
+#@export var tiles: Array[Array[int]]
+@export var dungeon_seed: int
+## The seed of the individual floor
+@export var floor_seed: int
+## Dungeon width and height, in tiles
+
+@export var dungeon_size: int
+## Width and height of the gridcells used to generate a dungeon floor
+@export var room_grid_size: int
+
+func _init(dungeon_paramaters: Dictionary, floor_number) -> void:
+	self.dungeon_seed = dungeon_paramaters.seed
+	self.dungeon_size = dungeon_paramaters.dungeon_size
+	self.room_grid_size = dungeon_paramaters.room_grid_size
+	
+	self.floor_seed = _generate_floor_seed()
+	_generate_floor()
+	
+func _generate_floor():
+	var floor_rng = RandomNumberGenerator.new()
+	floor_rng.seed = floor_seed
+	
+
+func _generate_floor_seed() -> int:
+	var dungeon_rng = RandomNumberGenerator.new()
+	dungeon_rng.seed = dungeon_seed
+	var new_seed = dungeon_rng.randi()
+	return new_seed
+
+func tiles_to_tilemap(tiles):
 	pass
--- src/Levels/Dungeon/dungeon.gd
+++ src/Levels/Dungeon/dungeon.gd
@@ -3,21 +3,42 @@ extends Resource
 ## A dungeon resource. Can be directly saved. Leave seed blank for a random seed.
 class_name Dungeon
 
+## The dungeon's seed. 
 @export var seed: int
-
 ## Width and height of the dungeon in tiles
 @export var dungeon_size: int
 ## The max amount of rooms per floor
 @export var maximum_rooms: int
 ## The total amount of floors
 @export var total_floors: int
+## An array storing all of the floors
+@export var floors: Array[Floor]
+## Stores the dungeon parameters to be passed into a level
+@export var dungeon_parameters: Dictionary
 var rng := RandomNumberGenerator.new()
+var room_grid_size
+
 
 const pregen_floors := 10
 func _init(seed := 0, dungeon_size := 1000, maximum_rooms := 100, total_floors := 100):
+	rng.seed = Time.get_unix_time_from_system()
+	self.seed = seed
 	self.dungeon_size = dungeon_size
+	self.maximum_rooms = maximum_rooms
+	self.room_grid_size = dungeon_size / floor(sqrt(self.maximum_rooms)) # how wide each room generation tile is
+	self.dungeon_parameters = {
+		"seed": self.seed,
+		"dungeon_size" : self.dungeon_size,
+		"room_grid_size":  self.room_grid_size
+	}
+	
 	if(seed==0):
 		self.seed = rng.randi_range(1, 1000)
 	else:
 		self.seed = clampi(seed, 1, 1000)
+		
+	for f in pregen_floors:
+		var new_floor = Floor.new(dungeon_parameters, f)
+		self.floors.append(new_floor)
+		
 	
